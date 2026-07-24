c3d73c0394399622ad99a76c2f7a1a2407e3da04
ramonDev <ramon.osorio1989@gmail.com>
2026-07-24 02:57:54 +0000

    Agreda funcion para confirma o cancelar una accion

--- modulos/herramientas.sh
+++ modulos/herramientas.sh
@@ -2,6 +2,7 @@
 
 #Script de herramientas administrativas de uso exclusivo para usuarios con privilegios 
 
+#REGISTRO DIARIO DE INGRESOS Y SALIDAS DEL SISTEMA
 registrar_bitacora(){
 
 local msj="$1"
@@ -11,6 +12,7 @@ echo "$timestamp" - "$(whoami)" - "$msj" >> "$ARCHIVO_BITACORA"
 
 }
 
+#PAUSAR LA PANTALLA
 pausar(){
 
 echo
@@ -18,6 +20,7 @@ read -rp " Presione ENTER para continuar..."
 
 }
 
+#VALIDACION PARA ACCIONES ADMINISTRATIVAS
 requiere_root(){
 
 if [[ "$ES_ROOT" != "si" ]]; then
@@ -26,5 +29,23 @@ if [[ "$ES_ROOT" != "si" ]]; then
 	return 1
 fi
 	return 0
+}
+
+#VALIDACION DE CONFIRMACION SI/NO
+confirmar_accion(){
+
+local mensaje="$1"
+local respuesta
+
+read -rp "$mensaje" (s/n): " respuesta
+
+if [[ "$respuesta" == "s " ]] || [[ "$respuesta" == "S" ]]; then
+
+	return 0
+else
+	echo " Cancelando operacion" 
+	return 1
+fi
+
 }
 
